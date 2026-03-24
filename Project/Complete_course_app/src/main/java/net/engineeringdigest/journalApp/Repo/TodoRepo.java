c7486c9ae38cdca92bc6510677b53554be2198eb
Samyak <123289857+SamyakBhaware@users.noreply.github.com>
2026-03-24 08:15:32 +0530

    Delete Project/Complete_course_app directory

--- Project/Complete_course_app/src/main/java/net/engineeringdigest/journalApp/Repo/TodoRepo.java
+++
@@ -1,9 +0,0 @@
-package net.engineeringdigest.journalApp.Repo;
-
-import net.engineeringdigest.journalApp.model.Todo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TodoRepo extends MongoRepository<Todo, String>{
-}
